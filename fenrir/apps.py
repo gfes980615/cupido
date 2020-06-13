727bcdc95ebaa182a76a2583bf6ea698d4023765
Brother <ltscyt0717@gmail.com>
2020-06-14 00:09:05 +0800

    create fenrir app

---
+++ fenrir/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FenrirConfig(AppConfig):
+    name = 'fenrir'
---
+++ fenrir/views.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from django.shortcuts import render
+import requests
+import re
+import MySQLdb
+from bs4 import BeautifulSoup
+import rate_tool.rate_tool as tool
+
+def home(request):
+    return render(request, 'fenrir/fenrir.html', {
+        'current_time': str(datetime.now()),
+    })
+
+class ExchangeRate:
+    country = ""           #國家
+    cashRateBuy = 0         #現金買入
+    cashRateSell = 0        #現金賣出
+    realtimeRateBuy = 0     #即期買入
+    realtimeRateSell = 0    #即期賣出
+    def __init__(self, country="", cashRateBuy=0, cashRateSell=0, realtimeRateBuy=0, realtimeRateSell=0):
+        self.country = country
+        self.cashRateBuy = cashRateBuy
+        self.cashRateSell = cashRateSell
+        self.realtimeRateBuy = realtimeRateBuy
+        self.realtimeRateSell = realtimeRateSell
+
+def getTaiwanBankRate():
+    url = "https://rate.bot.com.tw/xrt?Lang=zh-TW"
+    req = requests.get(url)
+    req.encoding = 'utf8'
+
+    soup = BeautifulSoup(req.text, "html.parser")
+    countryCoins = soup.select("td.currency.phone-small-font")  #取得幣別
+    countrys = []
+    
+    countCountry = 0
+    for countryCoin in countryCoins:
+        country = countryCoin.select("div.hidden-phone.print_show")
+        countrys.append(country[0].text.strip())
+        countCountry = countCountry + 1
+
+    CashRates = soup.select("td.rate-content-cash.text-right.print_hide")  #取得現金匯率
+    RealtimeRates = soup.select(
+        "td.rate-content-sight.text-right.print_hide")  #取得即期匯率
+
+    cashRateBuy, cashRateSell = tool.GetRateResult(CashRates)
+
+    realtimeRateBuy, realtimeRateSell = tool.GetRateResult(RealtimeRates)
+
+    rate_list = []
+    
+    for i in range(len(countrys)):
+        rate_obj = ExchangeRate(countrys[i],cashRateBuy[i],cashRateSell[i],realtimeRateBuy[i],realtimeRateSell[i])
+        rate_list.append(rate_obj)
+        
+
